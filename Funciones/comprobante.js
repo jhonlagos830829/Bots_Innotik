dfdbd41c9a63670b2114bab90a0eccb219874de0
Jhon Lagos <jhonlagosx@hotmail.com>
2024-10-30 01:10:31 -0500

    Con módulo de verificación de pagos

--- Funciones/comprobante.js
+++ Funciones/comprobante.js
@@ -403,7 +403,7 @@ async function extraerDatosNequi(texto){
             
             //Extraer la conversación de la línea de conversación
             let lineaConversacion = texto.match(ExpRegConversacionNequi)[0].replaceAll('\n', ' ').replaceAll('¿', '')
-            comprobante.conversacion = lineaConversacion.substring(lineaConversacion.indexOf(' ')).replaceAll('  ', ' ').trim()
+            comprobante.conversacion = lineaConversacion.substring(lineaConversacion.indexOf(' ')).toLowerCase().replaceAll('  ', ' ').trim()
 
         }
         
