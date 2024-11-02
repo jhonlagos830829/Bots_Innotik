5422ed89e448952e6208871d70856d8b6db8c292
Jhon Lagos <jhonlagosx@hotmail.com>
2024-11-01 21:27:19 -0500

    Con módulo de verificación de pagos

--- Funciones/pruebasCodigo.js
+++ Funciones/pruebasCodigo.js
@@ -1,11 +1,13 @@
-let fechaCadena = '02-10-2024 06:39 am'
-fechaCadena = fechaCadena.replaceAll('-', ' ')
-console.log('Dia:|' + fechaCadena.split(' ')[0] + '|')
-console.log('Mes:|' + fechaCadena.split(' ')[1] + '|')
-console.log('Año:|' + fechaCadena.split(' ')[2] + '|')
+let fechaCadena = "2024-10-03T23:08:51.299Z"
+// fechaCadena = fechaCadena.replaceAll('-', ' ')
+// console.log('Dia:|' + fechaCadena.split(' ')[0] + '|')
+// console.log('Mes:|' + fechaCadena.split(' ')[1] + '|')
+// console.log('Año:|' + fechaCadena.split(' ')[2] + '|')
 
-let fecha = new Date(fechaCadena)
-fecha.setDate(fechaCadena.split(' ')[0])
-fecha.setMonth(fechaCadena.split(' ')[1] - 1)
-console.log('La fecha es')
-console.log(fecha)
+// let fecha = new Date(fechaCadena)
+// fecha.setDate(fechaCadena.split(' ')[0])
+// fecha.setMonth(fechaCadena.split(' ')[1] - 1)
+// console.log('La fecha es')
+// console.log(fecha)
+let nuevaFecha = new Date(fechaCadena)
+console.log(nuevaFecha.toLocaleString('es-ES', { hour12: true }))
